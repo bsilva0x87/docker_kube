d64fcb1509cc07e01735b658ac84e884e4a6e6c8
Bruno Silva <bruno.silva@deliverycenter.com>
2020-02-12 17:12:08 -0300

    routes scope and factory author updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :books, only: %i[index show]
-  resources :authors, only: %i[index]
+  scope 'api/v1' do
+    resources :books, only: %i[index show]
+    resources :authors, only: %i[index]
+  end
 end
